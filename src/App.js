ece0f97903c61d04ca2c753dfb16a49790e0fcc3
Paramveer Singh <paramsinghkhattra@gmail.com>
2023-08-08 10:56:53 +0530

    Ep-04 completed.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./header/Header";
 
-function App() {
-  return <>Test</>;
-}
-
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 export default App;
---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="" alt="logo" />
+      </div>
+      <div className="searchBar"></div>
+      <div className="cart"></div>
+    </div>
+  );
+};
+
+export default Header;
