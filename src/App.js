0777f7e84ce1c59034c7d871e47d142b339791ca
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-01-15 23:17:19 +0530

    Added React form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./product/Product";
 import Page404 from "./components/page-404/Page404";
 import RestaurantDetails from "./main/restraunts/restaurant-details/RestaurantDetails";
+import { User } from "./user/User";
+import Register from "./user/register/Register";
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
         <Routes>
           <Route path="restaurant/:id" element={<RestaurantDetails />} />
           <Route path="product/:id" element={<Product />} />
+          <Route path="user" element={<User />} />
+          <Route path="signup" element={<Register />} />
           <Route path="/" element={<Main />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
