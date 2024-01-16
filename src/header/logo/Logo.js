be62cb4fe7c7058cbf254e9075b5d6c0c778891b
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-01-16 18:53:54 +0530

    Header Logo connected to a link

--- src/header/logo/Logo.js
+++ src/header/logo/Logo.js
@@ -1,15 +1,16 @@
 import React from "react";
 import imgUrl from "../../assets/images/logo.jpg";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <div className="flex justify-start items-center cursor-pointer">
+    <Link to={"/"} className="flex justify-start items-center cursor-pointer">
       <img src={imgUrl} alt="AZFoodLogo" className="m-2 w-5 h-auto lg:w-20 " />{" "}
       <span className="mx-1 text-xl lg:text-3xl lg:mx-3 text-red-600">
         {" "}
         AZ Foods
       </span>
-    </div>
+    </Link>
   );
 };
 
