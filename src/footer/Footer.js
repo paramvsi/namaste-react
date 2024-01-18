d77dd1f870a0a083303eddb9937100e9b7983a06
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-01-18 18:16:34 +0530

    Login Page and Footer Fixed

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { FaSnapchat } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="bg-black w-full flex justify-center  items-start text-white p-5 lg:p-12">
       <div className="m-7">
         <p className="text-xl">AZ Foods</p>
-        &copy; azfoods industries pvt inc.
+        &copy; azfoods industries.
       </div>
       <div className="m-7">
         <p className="text-lg">Follow us on</p>
-        <div>Twitter . SnapChat . Instagram . Facebook</div>
+        <div className="flex flex-col lg:flex-row justify-start gap-3 items-start lg:items-center ">
+          <p className="flex gap-[2px] items-center">
+            <FaFacebook /> Facebook
+          </p>
+          <p className="flex gap-[2px] items-center">
+            <FaXTwitter /> Twitter
+          </p>
+          <p className="flex gap-[2px] items-center">
+            <FaYoutube /> Youtube
+          </p>
+          <p className="flex gap-[2px] items-center">
+            <FaSnapchat /> Snapchat
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/user/login/Login.js
+++ src/user/login/Login.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { handleSubmit, control } = useForm();
+
+  const onSubmit = (data) => {
+    // Perform login logic here with the form data
+    console.log("Login data:", data);
+  };
+  return (
+    <div className="w-[95%] h-screen lg:h-[500px] flex items-center justify-center m-auto">
+      <form
+        className="border-2 border-gray-700 p-8 rounded-lg flex items-center flex-col w-screen lg:w-96"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h2 className="text-xl text-center font-bold text-gray-800 p-7">
+          Login to enjoy your treats..
+        </h2>
+
+        <Controller
+          name="phoneNumber"
+          control={control}
+          rules={{ required: "Phone Number is required" }}
+          render={({ field, fieldState }) => (
+            <>
+              <input
+                className="w-[90%] px-5 py-2 text-lg border-2 border-gray-600"
+                type="number"
+                {...field}
+                placeholder="Phone Number"
+              />
+              {fieldState.error && (
+                <p className="text-red-600">{fieldState.error.message}</p>
+              )}
+            </>
+          )}
+        />
+
+        <button
+          className="w-[90%] p-2 text-lg bg-red-600  text-white my-4"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
