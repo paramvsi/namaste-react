fb7177ea5ca7f167319ba7d5aa12316b9f615f68
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-01-25 21:08:59 +0530

    Search form

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
-  return <div>Search</div>;
+  const handleSearch = (value) => {
+    console.log(value);
+  };
+  return (
+    <div>
+      <form
+        onSubmit={handleSearch}
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <input
+          type="text"
+          placeholder="Search..."
+          style={{ marginRight: "8px", padding: "8px" }}
+        />
+        <button
+          type="submit"
+          style={{ background: "transparent", border: "none" }}
+        >
+          <FaSearch style={{ fontSize: "1.2em" }} />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Search;
