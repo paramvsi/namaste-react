ca7e9620501fa68e4a65449b93086c54080ab609
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-01-27 20:08:28 +0530

    Cart Count Added

--- src/header/navbar/Navbar.js
+++ src/header/navbar/Navbar.js
@@ -5,6 +5,7 @@ import { MdOutlineSupportAgent } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { FaCartPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useCart } from "../../cart/CardContext";
 
 const Navbar = () => {
   const links = [
@@ -12,18 +13,24 @@ const Navbar = () => {
     { component: <TbDiscount2 />, name: "Offers", to: "/offers" },
     { component: <MdOutlineSupportAgent />, name: "Help", to: "/help" },
     { component: <FaUser />, name: "Sign In", to: "/login" },
-    { component: <FaCartPlus />, name: "Cart", to: "/cart" },
+    { component: <FaCartPlus />, name: "Cart", to: "/cart", showBanner: true },
   ];
+
+  const { cartItems } = useCart();
   return (
     <nav className="flex justify-between items-center ">
       {links.map((link) => (
         <Link key={link.name} to={link.to}>
           <div className="flex items-center text-sm mx-2 text-gray-800 lg:text-xl lg:mx-6 cursor-pointer hover:text-red-600">
             {link.component}
-            <span className="hidden lg:block lg:mx-2">{link.name}</span>
+            <span className="hidden lg:block lg:ml-2">{link.name}</span>
+            {link.showBanner && cartItems && (
+              <sup className="text-red-600 font-extrabold">
+                {cartItems.length}
+              </sup>
+            )}
           </div>
         </Link>
-        // <Link key={link.name} name={link.name} component={link.component} />
       ))}
     </nav>
   );
