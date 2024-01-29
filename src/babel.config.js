70d7c8a3c131fbaca5000d89350783598678abee
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-01-29 19:22:35 +0530

    More config changes for jest setup

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+  presets: [
+    [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      { targets: { node: "current" } },
+    ],
+  ],
 };
