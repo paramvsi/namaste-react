75e871162be01705ff9546b3d8785de6c399dc56
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-01-30 20:25:38 +0530

    Logo Img fixes

--- src/header/logo/Logo.js
+++ src/header/logo/Logo.js
@@ -1,11 +1,18 @@
 import React from "react";
-import imgUrl from "../../assets/images/logo.jpg";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to={"/"} className="flex justify-start items-center cursor-pointer">
-      <img src={imgUrl} alt="AZFoodLogo" className="m-2 w-5 h-auto lg:w-20 " />{" "}
+    <Link
+      data-testId={"logo"}
+      to={"/"}
+      className="flex justify-start items-center cursor-pointer"
+    >
+      <img
+        src={require("../../assets/images/logo.jpg")}
+        alt="AZFoodLogo"
+        className="m-2 w-5 h-auto lg:w-20 "
+      />{" "}
       <span className="mx-1 text-xl lg:text-3xl lg:mx-3 text-red-600">
         {" "}
         AZ Foods
